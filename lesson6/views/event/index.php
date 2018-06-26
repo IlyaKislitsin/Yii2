34335a8a85e3073f063ad34effe29b3c1f4ddb20
IlyaKislitsin <ilya.kislitsin@gmail.com>
2018-06-26 19:53:33 +0500

    ДЗ к 6-ому уроку

---
+++ lesson6/views/event/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Events';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="event-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php Pjax::begin(); ?>
+
+    <p>
+        <?= Html::a('Create Event', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            
+            'id',
+            'text:ntext',
+            'dt',
+            'creator_id',
+            'created_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+    <?php Pjax::end(); ?>
+</div>
